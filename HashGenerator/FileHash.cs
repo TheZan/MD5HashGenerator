268556f2c21bc25e55bf61b9cea9b74330de8263
TheZan <sochnev66@gmail.com>
2020-09-12 13:20:48 +0300

    release project

---
+++ HashGenerator/FileHash.cs
@@ -0,0 +1,8 @@
+﻿namespace HashGenerator
+{
+    public class FileHash
+    {
+        public string FileName { get; set; }
+        public string Hash { get; set; }
+    }
+}
---
+++ HashGenerator/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text.Json;
+
+namespace HashGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите путь к папке с файлами, для которых нужно сгенерировать хэш:");
+            string path = Console.ReadLine();
+
+            List<string> files = DirSearch(path);
+
+            List<FileHash> hashes = new List<FileHash>();
+
+            Console.WriteLine("Список файлов:");
+
+            foreach (var file in files)
+            {
+                hashes.Add(new FileHash()
+                {
+                    FileName = file.Substring(file.LastIndexOf("\\") + 1, file.Length - file.LastIndexOf("\\") - 1),
+                    Hash = CalculateMD5(file)
+                });
+
+                Console.WriteLine(file);
+            }
+
+            GenerateJson(hashes);
+
+            Console.ReadKey();
+        }
+
+        static List<string> DirSearch(string sDir)
+        {
+            List<string> files = new List<string>();
+            try
+            {
+                foreach (string f in Directory.GetFiles(sDir))
+                {
+                    files.Add(f);
+                }
+
+                foreach (string d in Directory.GetDirectories(sDir))
+                {
+                    files.AddRange(DirSearch(d));
+                }
+            }
+            catch (Exception excpt)
+            {
+                Console.WriteLine(excpt.Message);
+            }
+
+            return files;
+        }
+
+        static string CalculateMD5(string filename)
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(filename))
+                {
+                    var hash = md5.ComputeHash(stream);
+                    return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+                }
+            }
+        }
+
+        static async void GenerateJson(List<FileHash> hashes)
+        {
+            try
+            {
+                using (FileStream fs = new FileStream("MD5Hash.json", FileMode.Create))
+                {
+                    await JsonSerializer.SerializeAsync(fs, hashes);
+                }
+
+                Console.WriteLine("Генерация завершена успешно. Создан файл MD5MD5Hash.json.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
